8550442f666463dcd94b954cfaa0cd0bc12468ad
Mark Benson <thealternator89@gmail.com>
2021-02-27 20:37:40 +1300

    Setup initial project structure

---
+++ Booster.CodingTest.MarkBenson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Booster.CodingTest.MarkBenson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
